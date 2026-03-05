224b537f6362f5df792e32d6d9fc579b1e359d40
dariopullia <dariopullia@yahoo.it>
2026-03-05 03:29:41 -0600

    fixed names

--- scripts/run_standard_mc_data.sh
+++ scripts/run_standard_mc_data.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 
-json_file=../analysis_settings/standard.json
+json_file=../analysis_settings/mc_data/standard.json
 python3 /exp/dune/app/users/dpullia/neutrino_search_selection/apps/mc_data_comparison.py -j $json_file
 
